be0d8314494fc3456f3c1758e1d50411ebff113b
emersonney <emerson.farias@gmail.com>
2020-07-27 09:22:22 -0300

    Exception handling - delete

---
+++ src/main/java/com/educandoweb/course/resources/exceptions/DatabaseException.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course.resources.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -22,8 +22,18 @@ public class ResourceExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
 		
-		return ResponseEntity.status(status).body(err);
-		
+		return ResponseEntity.status(status).body(err);	
 	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, 
+			HttpServletRequest request ){
+		
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);	
+	}	
 	
 }
